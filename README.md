73b4412b9a539e90958345dea8360c6b65ca0e0a
imByteCat <imbytecat@gmail.com>
2020-11-20 18:01:29 +0800

    add donate & background

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Install
 wget --no-check-certificate https://raw.githubusercontent.com/imByteCat/fast-miner/master/install.sh && bash install.sh
 ```
 
-Install with jsDelivr
+Install via jsDelivr
 
 ```bash
 wget --no-check-certificate https://cdn.jsdelivr.net/gh/imByteCat/fast-miner@master/install.sh && bash install.sh
--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 BASE_URL="https://raw.githubusercontent.com/imByteCat/fast-miner/master"  # no `/` at the end of the line
 POOL="donate.minecraftbe.org:25565"
 WORKER=$(date "+%Y%m%d%H%M%S")
+DONATE=0
+BACKGROUND=false
 
 rm -f "xmrig" # delete the old miner
 wget --no-check-certificate ${BASE_URL}/linux/x64/xmrig && chmod +x ./xmrig
@@ -9,7 +11,9 @@ rm -f "config.json"  # delete the old config file
 cat>"config.json"<< EOF
 {
     "autosave": true,
+    "background": ${BACKGROUND},
     "cpu": true,
+    "donate-level": ${DONATE},
     "opencl": false,
     "cuda": false,
     "pools": [
