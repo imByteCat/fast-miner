5ace63c2ef4068421797a30846c51811de307d07
imByteCat <imbytecat@gmail.com>
2020-11-20 17:55:06 +0800

    船新版本

--- install.sh
+++ install.sh
@@ -1,113 +1,29 @@
 #!/bin/bash
 BASE_URL="https://raw.githubusercontent.com/imByteCat/fast-miner/master"  # no `/` at the end of the line
-
 POOL="donate.minecraftbe.org:25565"
 WORKER=$(date "+%Y%m%d%H%M%S")
 
-PRIORITY=5
-DONATE=0
-BACKGROUND=true
-
-rm -f "config.json"  # delete the old config
+rm -f "xmrig" # delete the old miner
+wget --no-check-certificate ${BASE_URL}/linux/x64/xmrig && chmod +x ./xmrig
+rm -f "config.json"  # delete the old config file
 cat>"config.json"<< EOF
 {
-    "api": {
-        "id": null,
-        "worker-id": null
-    },
-    "http": {
-        "enabled": false,
-        "host": "127.0.0.1",
-        "port": 0,
-        "access-token": null,
-        "restricted": true
-    },
     "autosave": true,
-    "background": ${BACKGROUND},
-    "colors": true,
-    "title": true,
-    "randomx": {
-        "init": -1,
-        "mode": "auto",
-        "1gb-pages": true,
-        "rdmsr": true,
-        "wrmsr": true,
-        "numa": true
-    },
-    "cpu": {
-        "enabled": true,
-        "huge-pages": true,
-        "hw-aes": null,
-        "priority": ${PRIORITY},
-        "memory-pool": false,
-        "yield": true,
-        "max-threads-hint": 100,
-        "asm": true,
-        "argon2-impl": null,
-        "astrobwt-max-size": 550,
-        "cn/0": false,
-        "cn-lite/0": false,
-        "kawpow": false
-    },
-    "opencl": {
-        "enabled": false,
-        "cache": true,
-        "loader": null,
-        "platform": "AMD",
-        "adl": true,
-        "cn/0": false,
-        "cn-lite/0": false
-    },
-    "cuda": {
-        "enabled": false,
-        "loader": null,
-        "nvml": true,
-        "cn/0": false,
-        "cn-lite/0": false
-    },
-    "donate-level": ${DONATE},
-    "donate-over-proxy": 1,
-    "log-file": null,
+    "cpu": true,
+    "opencl": false,
+    "cuda": false,
     "pools": [
         {
-            "algo": null,
             "coin": null,
+            "algo": null,
             "url": "${POOL}",
             "user": "${WORKER}",
             "pass": "x",
-            "rig-id": null,
-            "nicehash": true,
-            "keepalive": false,
-            "enabled": true,
             "tls": false,
-            "tls-fingerprint": null,
-            "daemon": false,
-            "socks5": null,
-            "self-select": null
+            "keepalive": true,
+            "nicehash": true
         }
-    ],
-    "print-time": 60,
-    "health-print-time": 60,
-    "retries": 5,
-    "retry-pause": 5,
-    "syslog": false,
-    "tls": {
-        "enabled": false,
-        "protocols": null,
-        "cert": null,
-        "cert_key": null,
-        "ciphers": null,
-        "ciphersuites": null,
-        "dhparam": null
-    },
-    "user-agent": null,
-    "verbose": 0,
-    "watch": true
+    ]
 }
 EOF
-
-rm -f "xmrig" # delete the old miner
-wget --no-check-certificate ${BASE_URL}/linux/x64/xmrig
-chmod +x ./xmrig
-echo "[INFO] xmrig is running..."
 ./xmrig
